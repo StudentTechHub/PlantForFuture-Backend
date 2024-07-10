d1033c75c02f4e09e89c9d8d681ea8ebf313a342
Love Khatri <lovekhatri9211@gmail.com>
2024-07-10 12:29:07 +0530

    ADD: cors and .env.example

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ import creatorRouter from "./routes/creator.route.js";
 import volunteerRouter from "./routes/volunteer.route.js";
 import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
+import cors from 'cors'; // Add this line
 dotenv.config();
+
+// CORS configuration
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors(corsOptions)); // Add this line
 
 app.get('/helloworld', (req, res) => {
     res.send('Hello World');
