3a17dc458d170a98cc546e47883c8154323caa46
Love Khatri <lovekhatri9211@gmail.com>
2024-07-11 15:17:57 +0530

    Remove httpOnly

--- utils/genTokenAndCookie.js
+++ utils/genTokenAndCookie.js
@@ -7,7 +7,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 
 	res.cookie("token", token, {
 		maxAge: 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+		// httpOnly: true, // prevent XSS attacks cross-site scripting attacks
 		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
 		secure: process.env.NODE_ENV == "production",
 	});
