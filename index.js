6afdcdca8ddcfc7e6bf92bac1ad7490a0f078015
Love Khatri <lovekhatri9211@gmail.com>
2024-07-30 21:34:40 +0530

    allow all origin in cors v3

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const corsOptions = {
     //         callback(new Error('Not allowed by CORS'));
     //     }
     // },
-    origin: "*",
+    origin: "https://plantforfuture.netlify.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
