1e7fc906a421958fb14d2628ffa449dac89a2ef2
Love Khatri <lovekhatri9211@gmail.com>
2024-07-30 21:54:05 +0530

    cookie fix v2

--- utils/genTokenAndCookie.js
+++ utils/genTokenAndCookie.js
@@ -6,10 +6,10 @@ const generateTokenAndSetCookie = (userId, creator, res) => {
 	});
 
 	res.cookie(creator ? "_creator_token" : "_volunteer_token", token, {
-		maxAge: 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "none", // CSRF attacks cross-site request forgery attacks
-		secure: true,
+		maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+		httpOnly: true,
+		secure: true, // Requires HTTPS
+		sameSite: "None" // Allows cross-site cookies
 	});
 
 	return token;
