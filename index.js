0e38e9fb7df0bb2e55731c540180d34adfd986b8
Neeraj <neerajantil.antil26@gmail.com>
2024-07-31 17:48:29 +0530

    Fix cors

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ import creatorRouter from "./routes/creator.route.js";
 import volunteerRouter from "./routes/volunteer.route.js";
 import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
-// import cors from 'cors'; // Add this line
+import cors from 'cors'; // Add this line
 import activityRouter from "./routes/activity.route.js";
 import Volunteer from "../models/volunteer.model.js";
 import Creator from '../models/creator.model.js';
 dotenv.config();
 
+// const corsOptions = {
+//     // origin: "https://plantforfuture.netlify.app",  // <---- Useless code
+//     origin: ["http://localhost:3000", "https://plantforfuture.netlify.app"],
+//     default: "https://plantforfuture.netlify.app",
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
+// };
+
 const corsOptions = {
-    // origin: "https://plantforfuture.netlify.app",   // <---- Useless code
-    origin: ["http://localhost:3000", "https://plantforfuture.netlify.app"],
-    default: "https://plantforfuture.netlify.app",
+    origin: "https://plantforfuture.netlify.app",
+    // origin: ["http://localhost:3000", "https://plantforfuture.netlify.app"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
@@ -23,9 +31,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// app.use(cors({
-//     ...corsOptions,
-// }));
+app.use(cors({
+    ...corsOptions,
+}));
 
 app.use(json());
 app.use(cookieParser());
@@ -35,11 +43,11 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.originalUrl}`);
 
     // const origin = corsOptions.origin.includes(req.header('origin').toLowerCase()) ? req.headers.origin : corsOptions.default;
-    const origin = corsOptions.default;
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Methods", corsOptions.methods.join(','));
-    res.header("Access-Control-Allow-Headers", corsOptions.allowedHeaders.join(','));
-    res.header("Access-Control-Allow-Credentials", "true");
+    // const origin = corsOptions.default;
+    // res.header("Access-Control-Allow-Origin", origin);
+    // res.header("Access-Control-Allow-Methods", corsOptions.methods.join(','));
+    // res.header("Access-Control-Allow-Headers", corsOptions.allowedHeaders.join(','));
+    // res.header("Access-Control-Allow-Credentials", "true");
     next();
 })
 
