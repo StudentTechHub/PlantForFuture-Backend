5d1024b1040442662a1a16d615d613c0bb0ff99e
Love Khatri <lovekhatri9211@gmail.com>
2024-07-31 11:25:25 +0530

    Fix cookie v6

--- utils/genTokenAndCookie.js
+++ utils/genTokenAndCookie.js
@@ -10,11 +10,11 @@ const generateTokenAndSetCookie = (userId, creator, res) => {
 	// 	httpOnly: true,
 	// 	partition: true,
 	// 	secure: true, // Requires HTTPS
-	// 	sameSite: "none", // Allows cross-site cookies
+	// 	sameSite: "None", // Allows cross-site cookies
 	// 	// domain: "plantforfuture.netlify.app", // Domain name only, no protocol
 	// 	// path: "/"                     // Root path
 	// });
-	res.setHeader('Set-Cookie', `${creator ? "_creator_token" : "_volunteer_token"}=${token}; Max-Age=${3600 * 24}; Path=/; HttpOnly; SameSite=None; Secure; Partitioned;`)
+	res.setHeader('Set-Cookie', `${creator ? "_creator_token" : "_volunteer_token"}=${token}; Max-Age=${3600 * 24}; Path=/; HttpOnly; SameSite=None; Secure; Partitioned;Priority=High;`)
 
 	return token;
 };
