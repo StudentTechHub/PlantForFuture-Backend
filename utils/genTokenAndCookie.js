20103b970196cf725451d18a02c740f44904b637
Love Khatri <lovekhatri9211@gmail.com>
2024-07-31 11:01:36 +0530

    Fix cookie v2

--- utils/genTokenAndCookie.js
+++ utils/genTokenAndCookie.js
@@ -7,8 +7,8 @@ const generateTokenAndSetCookie = (userId, creator, res) => {
 
 	res.cookie(creator ? "_creator_token" : "_volunteer_token", token, {
 		maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
-		httpOnly: true,
-		secure: true, // Requires HTTPS
+		// httpOnly: true,
+		// secure: true, // Requires HTTPS
 		sameSite: "none", // Allows cross-site cookies
 		// domain: "plantforfuture.netlify.app", // Domain name only, no protocol
 		path: "/"                     // Root path
